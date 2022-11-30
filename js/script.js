9be8f28ac224cb84ece0659bc7ce15f8fcbb8e06
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-11-30 19:47:20 +0000

    dlatl1

--- js/script.js
+++ js/script.js
@@ -11,13 +11,34 @@
  */
 
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Template-PWA/sw.js", {
-    scope: "/ICS2O-Template-PWA/",
+  navigator.serviceWorker.register("/ICS2O-Assignment-4-JS/sw.js", {
+    scope: "/ICS2O-Assignment-4-JS/",
   })
 }
 
 // function description
 
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function update() {
+  console.log("update")
+  // input
+  const extraLarge = document.getElementById("extra-large").checked
+  const large = document.getElementById("large").checked
+  const medium = document.getElementById("medium").checked
+  const oneTopping = document.getElementById("1-topping").checked
+  const twoTopping = document.getElementById("2-topping").checked
+  const threeTopping = document.getElementById("3-topping").checked
+
+  if (extraLarge == true) {
+    console.log("size3")
+    const size = 10
+  } else if (large == true) {
+    const size = 8
+    console.log("size2")
+  } else if (medium == true) {
+    const size = 6
+    console.log("size1")
+  } else {
+    document.getElementById("answer").innerHTML =
+    "Please pick a size of the pizza"
+  }
 }
